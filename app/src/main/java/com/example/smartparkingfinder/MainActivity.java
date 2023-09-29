b41995526f51644543649e0cc3d221572840d96b
Lee Li Rong <p22014675@student.newinti.edu.my>
2023-09-29 17:46:48 +0800

    User Part 1

--- app/src/main/java/com/example/smartparkingfinder/MainActivity.java
+++ app/src/main/java/com/example/smartparkingfinder/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.smartparkingfinder;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
@@ -15,37 +18,72 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private UserAdapter adapter;
+    private RecyclerView mRecyclerView,HorizontalRV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DatabaseReference databaseReference;
+
+        // Initialize Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("location");
         FirebaseApp.initializeApp(this);
+        mRecyclerView = findViewById(R.id.verticalRecyclerView_Main);
+        HorizontalRV = findViewById(R.id.horizontalRecyclerView_Main);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        adapter = new UserAdapter(this, new ArrayList<>());
+        mRecyclerView.setAdapter(adapter);
+        HorizontalRV.setAdapter(adapter);
+        // Set an item click listener for the adapter
+        adapter.setOnItemClickListener(new UserAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(locationRVModel item) {
 
-        // Initialize the database reference
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("test").child("Space");
-
-        databaseReference.addValueEventListener(new ValueEventListener() {
+            }
+        });
+        // Retrieve data from Firebase
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String spaceStatus = dataSnapshot.getValue(String.class);
+                List<locationRVModel> locationDataList = new ArrayList<>();
 
-                // Update the UI based on the value of spaceStatus
-                TextView textViewSpaceStatus = findViewById(R.id.txt_Parking);
-                if ("Occupied".equals(spaceStatus)) {
-                    textViewSpaceStatus.setTextColor(Color.RED);
-                } else if ("Empty".equals(spaceStatus)) {
-                    textViewSpaceStatus.setTextColor(Color.GREEN); // or any other color
+                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
+                    String locationId = locationSnapshot.getKey(); // Get the location ID
+
+                    // Access the "details" node under the location ID
+                    DataSnapshot detailsSnapshot = locationSnapshot.child("details");
+
+                    String name = detailsSnapshot.child("name").getValue(String.class);
+                    String description = detailsSnapshot.child("description").getValue(String.class);
+                    Integer parkingAvailabilityObj = detailsSnapshot.child("parkingAvailability").getValue(Integer.class);
+                    String imageURL = detailsSnapshot.child("imageURL").getValue(String.class);
+
+                    int parkingAvailability = (parkingAvailabilityObj != null) ? parkingAvailabilityObj.intValue() : 0;
+
+                    if (locationId != null && name != null && description != null) {
+                        Log.d("Get Location", "Got Value");
+                        locationRVModel locationData = new locationRVModel(locationId, name, description, parkingAvailability, imageURL);
+                        locationDataList.add(locationData);
+                    }
                 }
+
+                // Update the RecyclerView adapter with the retrieved data
+                adapter.setData(locationDataList);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Handle error if necessary
+                // Handle errors here
             }
         });
     }
---
+++ app/src/main/java/com/example/smartparkingfinder/UserAdapter.java
@@ -0,0 +1,91 @@
+package com.example.smartparkingfinder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+    private List<locationRVModel> data;
+    private Context context;
+    private UserAdapter.OnItemClickListener listener;
+
+    public UserAdapter(Context context, List<locationRVModel> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(locationRVModel item);
+    }
+
+    public void setOnItemClickListener(UserAdapter.OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.location_rv, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        locationRVModel locationRVModel = data.get(position);
+        holder.locationTextView.setText(locationRVModel.getName());
+        holder.descriptionTextView.setText(locationRVModel.getDescription());
+        holder.parkingAvailabilityTextView.setText("Parking Availability: " + locationRVModel.getParkingAvailability());
+
+        // Load and display the image using Glide or a similar image loading library
+        Glide.with(context)
+                .load(locationRVModel.getImageURL())
+                .into(holder.locationImageView);
+
+        // Set an item click listener for the item view
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onItemClick(locationRVModel);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public void setData(List<locationRVModel> locationDataList) {
+        this.data = locationDataList;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView locationImageView;
+        TextView locationTextView;
+        TextView descriptionTextView;
+        TextView parkingAvailabilityTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            locationImageView = itemView.findViewById(R.id.idImgLocation);
+            locationTextView = itemView.findViewById(R.id.idLocation);
+            descriptionTextView = itemView.findViewById(R.id.idLocationDesc);
+            parkingAvailabilityTextView = itemView.findViewById(R.id.idParkingAvailable);
+        }
+    }
+
+
+}
